06bb45eec13e48a66a0cd7153c9901255970316a
Minaë Tani <minae.tani@icloud.com>
2022-11-10 17:09:59 +0200

    created button component for deployment test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from './components/Button.jsx';
 
 function App() {
+ 
   return (
-    
+    <Button />
   );
-}
+  }
 
 export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Button = () => {
+  
+    return (
+      <>
+        <button type='button' className='btn' />
+      </>
+    );
+    }
+
+ export default Button;
