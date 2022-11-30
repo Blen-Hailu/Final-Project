54ae0ca9a4c894d00691f7e3a34959097cd7cf6a
Minaë Tani <minae.tani@icloud.com>
2022-11-30 14:40:33 +0200

    WIP - image upload form

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const ImageUpload = () => {
- 
+  const [img, setImg] = useState('');
+  const handleImgChange = async (event) => {
+    const fileContent = encodeURIComponent (await event.target.files[0].text());
+    setImg(fileContent);
+    console.log(fileContent);
+  }
+
 return(
     <label>
     <input type="file" accept = "image/png, image/jpeg,.pdf" /> 
   </label>
   )
 }
+
+
