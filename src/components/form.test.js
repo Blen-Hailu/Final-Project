9316d20a5e5247172ac36fec4517e2666654b8ac
Blen-Hailu <belaki2@gmail.com>
2022-12-05 12:41:57 +0200

    deleted test pages that duplicated deleted test pages that was for pages

--- src/components/form.test.js
+++ src/components/form.test.js
@@ -1,10 +1,17 @@
-/*import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import RegistrationForm from './form';
 
-test('renders correctly', () => {
+/*test('renders correctly', () => {
   render(
       <RegistrationForm/>
     );
-  const nameElement = screen.getAllByRole('textbox');
+  const nameElement = screen.get('textbox');
   expect(nameElement).toBeInTheDocument();
+});*/
+
+
+test('renders learn react link', () => {
+  render(<RegistrationForm />);
+  const linkElement = screen.getByText(/register/i);
+  expect(linkElement).toBeInTheDocument();
 });
