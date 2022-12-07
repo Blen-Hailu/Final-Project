dfbe1b9baff060d113112852e3a5f748258e2a0b
Minaë Tani <minae.tani@icloud.com>
2022-12-07 11:41:00 +0200

    Deployment test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 test('renders text', () => {
-  render(<App />, { wrapper: BrowserRouter });
+  render(<Router><App /></Router>);
   const linkElement = screen.getByText(/Minae/i);
   expect(linkElement).toBeInTheDocument();
 });
